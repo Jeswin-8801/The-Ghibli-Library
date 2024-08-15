e89035073bf26e7c91e91b85ca07d35a16dad3f6
Jeswin-8801 <jeswin.santosh@outlook.com>
2024-08-15 21:59:06 +0530

    add new json payloads

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import GhibliIcon from "./assets/studio-ghibli-logo.svg";
 import MovieCard from "./components/MovieCard";
